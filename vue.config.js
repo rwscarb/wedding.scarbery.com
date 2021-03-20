9ae47ab6e56cd0ee1381a360f0d041b62b4d65bf
Ryan Scarbery <ryan.scarbery@gmail.com>
2021-03-20 12:37:02 -0500

    Set page title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Smart Wedding Contract';
+      return args;
+    })
   }
 }
