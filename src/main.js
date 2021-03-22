adb829e7008b46c74369e317ecb915d2558c29af
Ryan Scarbery <ryan.scarbery@gmail.com>
2021-03-22 11:17:19 -0500

    Testing move Sentry

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+
 import Vue from "vue";
+import VueRouter from "vue-router";
+
 import App from "./App.vue";
 import store from "./store";
-import VueRouter from "vue-router";
 import drizzleVuePlugin from "@drizzle/vue-plugin";
 import drizzleOptions from "./drizzleOptions";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
@@ -11,6 +15,15 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import { router } from "@/routes.js"
 import AsyncButton from '@/components/base/AsyncButton.vue';
+
+
+Sentry.init({
+  Vue: Vue,
+  dsn: "https://71d62fd28c8546c884b4fa43bd7696a8@o556458.ingest.sentry.io/5687401",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+  maxBreadcrumbs: 1,
+});
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+
 module.exports = {
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+      new SentryWebpackPlugin({
+        // sentry-cli configuration
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "ryan-scarbery",
+        project: "ryan-scarbery",
+
+        // webpack specific configuration
+        include: "./dist",
+        ignore: ["node_modules", "webpack.config.js"],
+      }),
+    ],
   },
   chainWebpack: config => {
     config.plugin('html').tap(args => {
