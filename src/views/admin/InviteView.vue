7024aed290f5278602604fcba55fdc53b477f093
Ryan Scarbery <ryan.scarbery@gmail.com>
2021-03-26 15:01:47 -0500

    Test Bug

--- src/views/admin/InviteView.vue
+++ src/views/admin/InviteView.vue
@@ -24,7 +24,7 @@
                     placeholder="0x"
                     required
                 ></v-text-field>
-                <v-btn type="submit" :loading="forms.invitation.loading" :disabled="!forms.invitation.valid">Invite</v-btn>
+                <v-btn type="submit" :loading="forms.invitation.loading" :disabled="!forms.invitation.valid" @click="giveMeSourceMapsSentry">Invite</v-btn>
             </v-form>
         </v-container>
     </div>
