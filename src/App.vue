1cbf200ff28b983ad929d0f093142981c5cf9b5c
Ryan Scarbery <ryan.scarbery@gmail.com>
2021-03-27 03:26:08 -0500

    Hide footer on mobile

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
             <div class="text-center">{{ snackbarMessage }}</div>
         </v-snackbar>
 
-        <v-footer padless app class="d-md-none">
+        <v-footer padless app class="d-none d-md-flex">
             <v-card class="text-center" width="100%">
                 <v-card-text>
                     <v-btn class="mx-4" icon href="https://twitter.com/ryan_scarbery" target="_blank">
