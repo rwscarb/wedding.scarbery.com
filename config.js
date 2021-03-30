959d6d68a89c90f2dfb632f72dbbd28442b7cee7
Ryan Scarbery <ryan.scarbery@gmail.com>
2021-03-30 13:27:06 -0500

    Any objections?

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
   spouse1: {
-    address: '0xb831c2b713307dd148cDE0797441da1e6ED2cEd8',
+    address: '0x8aFc344526Da71Ad9AdA5FCC076a35AB108DDF49',
     firstName: 'Ryan',
     lastName: 'Scarbery'
   },
   spouse2: {
-    address: '0x8aFc344526Da71Ad9AdA5FCC076a35AB108DDF49',
+    address: '0xb831c2b713307dd148cDE0797441da1e6ED2cEd8',
     firstName: 'Traci',
     lastName: 'Johan'
   },
